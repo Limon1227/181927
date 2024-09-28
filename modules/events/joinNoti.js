00adb6c9440bd632b00cd59e02cfbe16417788ea
Limon1227 <125444273+Limon1227@users.noreply.github.com>
2024-09-28 14:52:21 +0600

    Update joinNoti.js

--- modules/events/joinNoti.js
+++ modules/events/joinNoti.js
@@ -51,7 +51,7 @@ module.exports.run = async function({ api, event, Users }) {
   if (event.logMessageData.addedParticipants && Array.isArray(event.logMessageData.addedParticipants) && event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
     //api.changeNickname(`𝗕𝗢𝗧 ${(!global.config.BOTNAME) ? "Buddy" : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
     
-    let gifUrl = 'https://i.imgur.com/4HMupHz.gif';
+    let gifUrl = 'https://i.imgur.com/CSiu9bJ.gif';
 let gifPath = __dirname + '/cache/join/join.gif';
 
 axios.get(gifUrl, { responseType: 'arraybuffer' })
